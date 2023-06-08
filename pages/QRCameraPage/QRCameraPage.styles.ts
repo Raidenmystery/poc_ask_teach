469ef3f2951937ae2a7b37331dd9b7030f92566b
Lagoon <lagoon.studios.ve@gmail.com>
2023-06-08 00:28:25 -0400

    QR code scanner integration

--- pages/QRCameraPage/QRCameraPage.styles.ts
+++ pages/QRCameraPage/QRCameraPage.styles.ts
@@ -1,10 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  barCodeBox: {
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 300,
+    overflow: "hidden",
+    borderRadius: 30,
+  },
+  scanner: {
+    height: 400,
+    width: 400,
+  },
+  mainText: {
+    fontSize: 16,
+    margin: 20,
+  },
+  margin10: {
+    margin: 10,
   },
 });
--- pages/QRCameraPage/index.tsx
+++ pages/QRCameraPage/index.tsx
@@ -1,12 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { styles } from './QRCameraPage.styles';
+import { useEffect, useState } from "react";
+import { BarCodeScanner } from "expo-barcode-scanner";
+
+import { styles } from "./QRCameraPage.styles";
+
+import { Button, View } from "react-native";
+import Paragraph from "../../src/atoms/Paragraph/index";
 
 export default function QRCameraPage() {
-  return (
-    <View style={styles.container}>
-      <Text>QRCamera Page</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
+  // --- Hooks ---------------------------------------------------------------------------------
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState("Not yet scanned");
+  // --- END: Hooks ---------------------------------------------------------------------------
+
+  // --- Local state ---------------------------------------------------------------------------
+  // --- END: Local state ----------------------------------------------------------------------
+
+  // --- Refs ----------------------------------------------------------------------------------
+  // --- END: Refs -----------------------------------------------------------------------------
+
+  // --- Redux ---------------------------------------------------------------------------------
+  // --- End: Redux ----------------------------------------------------------------------------
+
+  // --- Side effects --------------------------------------------------------------------------
+  useEffect(() => {
+    setScanned(false);
+    askForCameraPermission();
+  }, []);
+  // --- END: Side Effects ---------------------------------------------------------------------
+
+  // --- Data and handlers ---------------------------------------------------------------------
+  const askForCameraPermission = async () => {
+    // (async () => {
+    const { status } = await BarCodeScanner.requestPermissionsAsync();
+    setHasPermission(status === "granted");
+    // })
+    // };
+  };
+
+  const handleBarCodeScanned = ({ data }: { type: any; data: string }) => {
+    setScanned(true);
+    setText(data);
+  };
+  // --- End: Data and handlers ----------------------------------------------------------------
+
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Paragraph>Requesting for camera Permission</Paragraph>
+      </View>
+    );
+  } else if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Paragraph style={styles.margin10}>No access to camera</Paragraph>
+        <Button title={"Allow camera"} onPress={() => askForCameraPermission()} />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <View style={styles.barCodeBox}>
+          <BarCodeScanner onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} style={styles.scanner} />
+        </View>
+        <Paragraph style={styles.mainText}>{text}</Paragraph>
+
+        {scanned && <Button title={"Scan again?"} onPress={() => setScanned(false)} />}
+      </View>
+    );
+  }
 }
