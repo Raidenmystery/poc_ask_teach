46910aaf7a31a12851f5870dd63dca95ef97b3d2
Lagoon <lagoon.studios.ve@gmail.com>
2023-06-08 22:43:21 -0400

    implement react-query to axiosTests page

--- App.tsx
+++ App.tsx
@@ -1,31 +1,38 @@
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import FormPage from './pages/FormPage';
-import ListPage from './pages/ListPage';
-import QRCameraPage from './pages/QRCameraPage';
-import AxiosTestPage from './pages/AxiosTests';
+import FormPage from "./pages/FormPage";
+import ListPage from "./pages/ListPage";
+import QRCameraPage from "./pages/QRCameraPage";
+import AxiosTestPage from "./pages/AxiosTests";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="List" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Form" component={FormPage} />
-        <Stack.Screen name="List" component={ListPage} />
-        <Stack.Screen name="QRCamera" component={QRCameraPage} />
-        <Stack.Screen name="AxiosTest" component={AxiosTestPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="AxiosTest" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Form" component={FormPage} />
+          <Stack.Screen name="List" component={ListPage} />
+          <Stack.Screen name="QRCamera" component={QRCameraPage} />
+          <Stack.Screen name="AxiosTest" component={AxiosTestPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+      {/* <ReactQueryDevtools initialIsOpen={false} position="bottom-right" /> */}
+    </QueryClientProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- pages/AxiosTests/index.tsx
+++ pages/AxiosTests/index.tsx
@@ -1,26 +1,43 @@
-import { useEffect } from "react";
-import { Text, View } from 'react-native';
-import { styles } from './AxiosTests.styles';
+import { Text, View } from "react-native";
+import { useQuery } from "react-query";
+import { styles } from "./AxiosTests.styles";
+
 import { pokeApi } from "../../services/pokeAPi";
 
 export default function AxiosTests() {
+  const { isLoading, data, isError, error } = useQuery(
+    "pokemon-list",
+    () => {
+      return pokeApi.get(`pokemon`);
+    },
+    {
+      staleTime: 30000, //if the data dont change often, the data stays the same for 30 secs. prevents unnecesaries fetchs
+    }
+  );
 
-  let pokemon: any = {};
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
-  const pokemons = () => {
-    pokeApi.get(`pokemon/ditto`).then((resp:any) => {
-      pokemon = resp.data;
-      console.log(resp.data.abilities[0])
-    });
-  };
-
-  useEffect(() => {
-    pokemons();
-  }, []);
+  if (isError) {
+    return (
+      <View style={styles.container}>
+        <Text>{(error as any).message || ""}</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <Text>{pokemon?.abilities?.[0]}</Text>
+      {data?.data.results.map((pokemon: any) => {
+        if (pokemon.name) {
+          return <Text>{pokemon.name}</Text>;
+        }
+      })}
     </View>
   );
 }
